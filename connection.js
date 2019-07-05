a41056f69f63c773c6bdb0a62b36a4889098bda4
Prashant Pawar <prashantpawar1000@gmail.com>
2019-07-05 21:43:36 +0530

    Fixed DB password varible mistake

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 var envName = process.env.NODE_ENV || 'dev'
-var conUrl = 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.PASSWORD + '@' + process.env.DB_URL + '/' + process.env.DB_NAME + '?retryWrites=true&w=majority'
+var conUrl = 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_URL + '/' + process.env.DB_NAME + '?retryWrites=true&w=majority'
 
 if (envName == "dev") {
     conUrl = 'mongodb://localhost:27017/' + process.env.DB_NAME
